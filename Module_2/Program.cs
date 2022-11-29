c5d888b595acffa20cbf70b6ba3616ce21ad3b16
Sergio Ebanatti <sergioebanatti@gmail.com>
2022-11-29 17:32:26 +0700

    initial

---
+++ Module_2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Module_2
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            string fullName = " Иванов Иван Иванович";
+            int age = 20;
+            string email = "ivanov@yandex.ru";
+            float iTScore = 80;
+            float mathScore = 65;
+            float physicsScore = 69;
+
+            float totalScore = iTScore + mathScore + physicsScore;
+            float gPA = totalScore / 3;
+
+
+
+            Console.WriteLine($"Ф.И.О.: {fullName} \nВозраст: {age} \nEmail: {email} \nБаллы по программированию: {iTScore} \nБаллы по математике: {mathScore} \nБаллы по физике: {physicsScore} \n\nЧтобы узнать баллы, нажмите любую клавишу");
+            Console.ReadKey(true);
+            Console.WriteLine($"\nСумма баллов: {totalScore} \nСредний балл: {(float)Math.Round(gPA, 2)}");
+            Console.ReadLine();
+        }
+
+    }
+}
